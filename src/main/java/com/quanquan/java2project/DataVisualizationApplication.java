07167abcba96557dc62bdc07734b29e48b51b96c
WLQ <836200779@qq.com>
2022-04-08 22:25:47 +0800

    rename

--- src/main/java/com/quanquan/java2project/DataVisualizationApplication.java
+++ src/main/java/com/quanquan/java2project/DataVisualizationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Java2ProjectApplication {
+public class DataVisualizationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Java2ProjectApplication.class, args);
+		SpringApplication.run(DataVisualizationApplication.class, args);
 	}
 
 }
--- src/test/java/com/quanquan/java2project/DataVisualizationApplicationTests.java
+++ src/test/java/com/quanquan/java2project/DataVisualizationApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Java2ProjectApplicationTests {
+class DataVisualizationApplicationTests {
 
 	@Test
 	void contextLoads() {
