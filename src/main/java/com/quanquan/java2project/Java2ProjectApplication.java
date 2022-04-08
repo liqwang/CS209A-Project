ff884f411d15a719ec686cd85da23ab4685d804f
WLQ <836200779@qq.com>
2022-04-08 22:11:16 +0800

    initialize

---
+++ src/main/java/com/quanquan/java2project/Java2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.quanquan.java2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java2ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quanquan/java2project/Java2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanquan.java2project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java2ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
