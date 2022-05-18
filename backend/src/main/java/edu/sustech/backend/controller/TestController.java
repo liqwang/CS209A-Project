ca6fba83d52965ae213ac0c87deedc2dc4e977cb
WLQ <836200779@qq.com>
2022-05-18 15:26:40 +0800

    Update

--- backend/src/main/java/edu/sustech/backend/controller/TestController.java
+++ backend/src/main/java/edu/sustech/backend/controller/TestController.java
@@ -6,7 +6,6 @@ import edu.sustech.search.engine.github.models.Entry;
 import edu.sustech.search.engine.github.models.repository.Repository;
 import edu.sustech.search.engine.github.models.repository.RepositoryResult;
 import edu.sustech.search.engine.github.models.user.User;
-import org.apache.ibatis.javassist.compiler.ast.Pair;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
